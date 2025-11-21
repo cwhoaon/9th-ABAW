e071a4a83f58b81d2ffff6f4c1cafaf482b0f79c
cwhoaon <kcwkcw0802@gmail.com>
2025-11-21 22:08:21 +0900

    minor bug fix

--- simple_processor/test.py
+++ simple_processor/test.py
@@ -2,10 +2,11 @@ from raw_process import RawProcess
 from pipeline import Pipeline
  
 data_path, data_name = "processed_data", "sample_video"
-device, video_path = "cuda:0", "raw/4.mp4"
+device = 'cuda'
+video_path = "raw/4.mp4" # path to input video file
 
 pipeline = Pipeline(device, data_path, data_name)
 result = pipeline(video_path)
 
-print("Pipeline output:", result.shape)
+print("Pipeline output:", result.shape) # emotion
 import ipdb; ipdb.set_trace()
